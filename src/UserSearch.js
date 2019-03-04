7cb2db5d3629683b8c4d59f1412b0cf358c4f5a5
Brona Ward <broshopping1@gmail.com>
2019-03-04 13:07:30 +0000

    Added styling and county selector to userSearch

--- src/UserSearch.js
+++ src/UserSearch.js
@@ -6,7 +6,10 @@ export default class UserSearch extends React.Component {
     state = { };
     render() {
       return (
-        <div className="container text-center Search-container">
+        <div className="container text-center search-container">
+        <h1 className="search-header">Feenix</h1>
+        <p className="app-decription text-muted">My App Description</p>
+        <div className="search-box">
         <div className="tab-content py-4 px-5">
         <div className="form-group">
         <input
@@ -15,8 +18,54 @@ export default class UserSearch extends React.Component {
           className="form-control"
         />
       </div>
+
+      <div className="form-group">
+        <select className="form-control" placeholder="County">
+          {[
+            "County",
+            "Antrim",
+            "Armagh",
+            "Carlow",
+            "Cavan",
+            "Clare",
+            "Cork",
+            "Derry",
+            "Donegal",
+            "Down",
+            "Dublin",
+            "Fermanagh",
+            "Galway",
+            "Kerry",
+            "Kildare",
+            "Kilkenny",
+            "Laois",
+            "Leitrim",
+            "Limerick",
+            "Longford",
+            "Louth",
+            "Mayo",
+            "Meath",
+            "Monaghan",
+            "Offaly",
+            "Roscommon",
+            "Sligo",
+            "Tipperary",
+            "Tyrone",
+            "Waterford",
+            "Westmeath",
+            "Wexford",
+            "Wicklow"
+          ]
+          // transforms the array of counties to an array of <option>county</option>
+          .map(county => (
+            <option>{county}</option>
+          ))}
+        </select>
+      </div>
+
       <div className="form-group">
       <button className="btn btn-dark">Search</button>
+      </div>
       </div>
       </div>
       </div>

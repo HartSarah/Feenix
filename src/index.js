a3874aa6ca2e2140b47591ef71a4c6ad21c47dcc
Allan Murray <47799563+AllanBMurray@users.noreply.github.com>
2019-03-11 17:42:33 +0000

    Add files via upload
    test index

--- src/index.js
+++ src/index.js
@@ -4,6 +4,12 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const express = require('express')
+const app = express()
+//app.get('/', (req, res) => res.send('Feenix App test server!?!?!?'))
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
+
+console.log("indexjs running");
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change

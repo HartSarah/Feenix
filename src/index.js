079fbc1f54bf1b6fccbcac26714574f04f63a29f
Sarah Hart <sarah.hart.158@gmail.com>
2019-03-20 16:56:26 +0000

    Fixed server error

--- src/index.js
+++ src/index.js
@@ -1,15 +1,16 @@
+
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-const express = require('express')
-const app = express()
+//const express = require('express')
+//const app = express()
 //app.get('/', (req, res) => res.send('Feenix App test server!?!?!?'))
-app.listen(3000, () => console.log('Example app listening on port 3000!'))
+//app.listen(3000, () => console.log('Example app listening on port 3000!'))
 
-console.log("indexjs running");
+//console.log("indexjs running");
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
